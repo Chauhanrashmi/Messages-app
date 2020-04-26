0730f292a476fbe725c5573efb0959653faa8d0d
Chauhanrashmi <chauhanrashmi1998@gmail.com>
2020-04-26 17:30:57 +0530

    angular frontend concepts applied

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent{
+  // posts=[
+  //   {
+  //     title:"First Message",content:"Here is your first message",description:"'The best is yet to be'"
+  //   },
+  //   {
+  //     title:"Second Message",content:"Here is your Second message",description:"Be thankful for what you have; you’ll end up having more"
+  //   },
+  //   {
+  //     title:"Third Message",content:"Here is your Third message",description:"Be thankful for what you have; you’ll end up having more"
+  //   }
+  // ];
+  posts=[];
+}
